ef940aac76b8dc21929d249f5ac583a8c3fdc803
SquallStrifeX <squallstrifex@gmail.com>
2024-01-30 16:49:21 +0100

    end exercise base

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php 
 
-class Movie {
+class Game {
     public $nome;
     public $genere;
     public $software_house;
     public $data;
-    public $candidato_goty;
+    public $console;
     public $prezzo_medio;
 
-    function __construct($_nome, $_genere, $_software_house, $_data, $_candidato_goty){
+    function __construct($_nome, $_genere, $_software_house, $_data, $_console){
         $this->nome = $_nome;
         $this->genere = $_genere;
         $this->software_house = $_software_house;
         $this->data = $_data;
-        $this->candidato_goty = $_candidato_goty;
+        $this->console = $_console;
         $this->prezzo_medio = $this->setPrezzo();
     }
 
@@ -33,6 +33,11 @@ class Movie {
         }
 }
 
+$game_1 = new Game('Final Fantasy X', 'JRPG', "Square-Soft", "2002", "Playstation 2", $prezzo_medio);
+$game_2 = new Game('Final Fantasy XVI', 'Action', "Square-Enix", "2023", "Playstation 5", $prezzo_medio);
+$game_3 = new Game('The Last of Us Parte II', 'Survivial/Horror', "Naughty Dog", "2020", "Playstation 4", $prezzo_medio);
+
+
 ?>
 
 
@@ -42,9 +47,31 @@ class Movie {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-12 m-5">
+                <div class="my-2"><?php echo $game_1->nome. " è un videogioco ". $game_1->genere. " sviluppato da ". $game_1->software_house. 
+                " e pubblicato nel ". $game_1->data. " per console ". $game_1->console. 
+                " al momento si trova in commercio ad un prezzo medio di ". $game_1->prezzo_medio. " euro. <br>"  ?>
+                </div>
+                <div class="my-2"><?php echo $game_2->nome. " è un videogioco ". $game_2->genere. " sviluppato da ". $game_2->software_house. 
+                " e pubblicato nel ". $game_2->data. " per console ". $game_2->console. 
+                " al momento si trova in commercio ad un prezzo medio di ". $game_2->prezzo_medio. " euro. <br>"  ?>
+                </div>
+                <div class="my-2"><?php echo $game_3->nome. " è un videogioco ". $game_3->genere. " sviluppato da ". $game_3->software_house. 
+                " e pubblicato nel ". $game_3->data. " per console ". $game_3->console. 
+                " al momento si trova in commercio ad un prezzo medio di ". $game_3->prezzo_medio. " euro. <br>"  ?>
+                </div>
+               
+                
+            </div>
+        </div>
+    </div>
+
+
 </body>
 </html>
